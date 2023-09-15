06c5870c604eace45054722ec84bc88712e7b1c6
sergio vergara <vansersgv@gmail.com>
2023-09-14 22:28:03 -0300

    segunda-entrega

---
+++ src/app.js
@@ -0,0 +1,144 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
+import { __dirname } from './path.js';
+import path from 'path';
+import mongoose from 'mongoose';
+
+import messageModel from './models/message.models.js';
+
+import routerProd from './routes/products.routes.js';
+import routerCart from './routes/carts.routes.js';
+import routerMessage from './routes/messages.routes.js';
+import productModel from './models/products.models.js';
+import cartModel from './models/carts.models.js';
+
+const app = express();
+
+const PORT = 8080;
+
+// Server
+const server = app.listen(PORT, () => {
+	console.log(`Servidor desde puerto: ${PORT}`);
+	console.log(`http://localhost:${PORT}`);
+});
+
+const io = new Server(server);
+
+//Middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', engine()); //defino que mi motor de plantillas va a ser handlebars
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname, './views'));
+
+// conexión con base de datos
+
+mongoose.connect('mongodb+srv://vansersgv:Azulado2023@cluster0.z2wu4ij.mongodb.net/?retryWrites=true&w=majority')
+	.then(() => console.log('DB conectada'))
+	.catch(error => console.log(`Error en conexión a MongoDB Atlas:  ${error}`));
+
+// Conexión con socket.io
+
+io.on('connection', socket => {
+	console.log('Conexión con Socket.io');
+
+	socket.on('load', async () => {
+		const data = await productModel.paginate({}, { limit: 5 });
+		socket.emit('products', data);
+	});
+
+	socket.on('previousPage', async page => {
+		const data = await productModel.paginate({}, { limit: 5, page: page });
+		socket.emit('products', data);
+	});
+
+	socket.on('nextPage', async page => {
+		const data = await productModel.paginate({}, { limit: 5, page: page });
+		socket.emit('products', data);
+	});
+
+	socket.on('addProduct', async data => {
+		const { pid, cartId } = data;
+		if (cartId) {
+			const cart = await cartModel.findById(cartId);
+			const productExists = cart.products.find(prod => prod.id_prod == pid);
+			productExists
+				? productExists.quantity++
+				: cart.products.push({ id_prod: pid, quantity: 1 });
+			await cart.save();
+			socket.emit('success', cartId);
+		} else {
+			const cart = await cartModel.create({});
+			cart.products.push({ id_prod: pid, quantity: 1 });
+			await cart.save();
+			socket.emit('success', cart._id.toString());
+		}
+	});
+
+	socket.on('loadCart', async cid => {
+		const cart = await cartModel.findById(cid);
+		socket.emit('cartProducts', cart.products);
+	});
+
+	socket.on('newProduct', async product => {
+		await productModel.create(product);
+		const products = await productModel.find();
+
+		socket.emit('products', products);
+	});
+
+	socket.on('mensaje', async info => {
+		const { email, message } = info;
+		await messageModel.create({
+			email,
+			message,
+		});
+		const messages = await messageModel.find();
+
+		socket.emit('mensajes', messages);
+	});
+});
+
+// Routes
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/static', (req, res) => {
+	res.render('index', {
+		rutaCSS: 'index',
+		rutaJS: 'index',
+	});
+});
+
+app.get('/static/realtimeproducts', (req, res) => {
+	res.render('realTimeProducts', {
+		rutaCSS: 'realTimeProducts',
+		rutaJS: 'realTimeProducts',
+	});
+});
+
+app.get('/static/chat', (req, res) => {
+	res.render('chat', {
+		rutaCSS: 'chat',
+		rutaJS: 'chat',
+	});
+});
+
+app.get('/static/products', (req, res) => {
+	res.render('products', {
+		rutaCSS: 'products',
+		rutaJS: 'products',
+	});
+});
+
+app.get('/static/carts/:cid', (req, res) => {
+	res.render('carts', {
+		rutaCSS: 'carts',
+		rutaJS: 'carts',
+	});
+});
+
+app.use('/api/products', routerProd); // defino que mi app va a usar lo que venga en routerProd para la ruta que defina
+app.use('/api/carts', routerCart);
+app.use('/api/messages', routerMessage);
---
+++ src/models/carts.models.js
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+
+const cartSchema = new Schema({
+	products: {
+		type: [
+			{
+				id_prod: {
+					type: Schema.Types.ObjectId, // tipo id autogenerado de mongoDb
+					ref: 'products',
+					required: true,
+				},
+				quantity: {
+					type: Number,
+					required: true,
+				},
+			},
+		],
+		default: function () {
+			return [];
+		},
+	},
+});
+
+/*
+const res = await cartModel
+ 	.findOne({ _id: '64f7bb1bf65706e542ebc0ab' })
+ 	.populate('products.id_prod');
+ console.log(JSON.stringify(res));
+
+Como buscar para que te traiga la info de la referencia
+*/
+
+cartSchema.pre('find', function () {
+	this.populate('products.id_prod');
+});
+// por defecto hace un populate en el find
+
+const cartModel = model('carts', cartSchema);
+
+export default cartModel;
